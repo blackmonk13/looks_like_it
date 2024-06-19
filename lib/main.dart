543de16c0205b1be2210c4e66d5b87bf707dbdfd
blackmonk13 <spencervalentino13@gmail.com>
2024-06-19 16:39:00 +0300

    init

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'app.dart';
+
+void main() {
+  runApp(
+    // For widgets to be able to read providers, we need to wrap the entire
+    // application in a "ProviderScope" widget.
+    // This is where the state of our providers will be stored.
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}

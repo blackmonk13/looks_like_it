57259e93ac68b34da1e57b0bb2f7142dc1e5ea29
blackmonk13 <spencervalentino13@gmail.com>
2024-06-21 04:22:09 +0300

    use layout

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:layout/layout.dart';
 import 'package:looks_like_it/pages/home_page.dart';
 
 class MyApp extends StatelessWidget {
@@ -7,16 +8,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Looks Like It',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blue,
+    return Layout(
+      format: MaterialLayoutFormat(),
+      child: MaterialApp(
+        title: 'Looks Like It',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.blue,
+          ),
+          useMaterial3: true,
         ),
-        useMaterial3: true,
+        home: const MyHomePage(),
       ),
-      home: const MyHomePage(),
     );
   }
 }
